f7f6ecbf56b480bd1e19db9615340dbcf4b157f5
Patrick <44551053+Forcebyte@users.noreply.github.com>
2019-05-14 18:45:01 -0400

    Update ZoomExport.ps1

--- ZoomExport.ps1
+++ ZoomExport.ps1
@@ -1,7 +1,10 @@
 $scriptPath = split-path -parent $MyInvocation.MyCommand.Definition
-$ErrorActionPreference = 'SilentlyContinue' | Out-Null﻿
+Write-host -foregroundcolor Green "Initializing Variables..."
+$ErrorActionPreference = 'SilentlyContinue' > $null
 # Removes all Variables in case we're running from ISE
 Get-Variable -Exclude PWD,*Preference | Remove-Variable -EA 0
+#ImportExcel is more useful than strictly modifying Excel COM Objects
+Install-Module ImportExcel -scope CurrentUser
 # Clears Screen
 Clear
 
@@ -80,18 +83,6 @@ $APIURL = "https://api.zoom.us/v2/metrics/meetings"
 $api_key = ''
 $api_secret = ''
 
-# Generate JWT key that is valid for a minute with the Key and Secret above
-$JWT = Generate-JWT -Algorithm 'HS256' -type 'JWT' -Issuer $api_key -SecretKey $api_secret -ValidforSeconds 60
-
-# Body of Invoke-webrequest
-$blob = @{
-   access_token = "$JWT"
-   type = "past"
-   from = "$EnteredStartDate"
-   to = "$EnteredEndDate"
-   next_page_token = ""
-   page_size = "300"
-}
 
 # Test to see if api_key and api_secret exist
 if ($api_key -eq ""){
@@ -111,6 +102,19 @@ if ($DateDifference -gt "32.00:00"){
 }elseif($DateDifference -lt "00.00:00"){
     Write-Warning "Date difference is Invalid, exiting."
     exit
+}
+
+# Generate JWT key that is valid for a minute with the Key and Secret above
+$JWT = Generate-JWT -Algorithm 'HS256' -type 'JWT' -Issuer $api_key -SecretKey $api_secret -ValidforSeconds 60
+
+# Body of Invoke-webrequest
+$blob = @{
+   access_token = "$JWT"
+   type = "past"
+   from = "$EnteredStartDate"
+   to = "$EnteredEndDate"
+   next_page_token = ""
+   page_size = "300"
 }
 
 
@@ -166,8 +170,9 @@ Write-host "End of data fetching portion - moving on to sorting"
    
    Additionally, we could use the Hostname, however that may conflict with other meetings                             #>
 $userlist = @(
-    "email1@email.com",
-    "email2@email.com",
+    "user1@email.com",
+    "user2@email.com",
+    "user3@email.com",
 )
 
 <#Loop through each of the entries sent back, and seperate a variable, Targettedmeetings, for requests that match the user list
@@ -177,17 +182,77 @@ Foreach ($meeting in $InitialMeetings){
         $targettedmeetings = $targettedmeetings + [array]$meeting
         }
     }
-#
 
-#Exports the targetted meetings into a CSV to where the script is, also exports all meetings for the date range to proof
+# try{$InitialMeetings | export-csv "$PSScriptRoot\Zoom Mass Export, $EnteredStartdate to $EnteredEndDate .csv" -NoTypeInformation}catch{write-error "Unrecoverable Error - Exception thrown"}
 
-try{$InitialMeetings | export-csv "$PSScriptRoot\Zoom Mass Export, $EnteredStartdate to $EnteredEndDate .csv" -NoTypeInformation}catch{write-error "Unrecoverable Error - Exception thrown"}
-try{$targettedmeetings | export-csv "$PSScriptRoot\Zoom Targeted Export, $EnteredStartdate to $EnteredEndDate .csv" -NoTypeInformation}catch{write-error "Unrecoverable Error - Exception thrown"}
+#Exports the Targeted meetings as two sheets in a single Excel worksheet file 
+$WorkItem = "$env:USERPROFILE\Downloads\Zoom Targeted Export, $EnteredStartdate to $EnteredEndDate.xlsx"
+#Coaches sheet
+try{$targettedmeetings | Select -Property "host","email","topic","start_time","end_time","duration","participants" | export-excel $WorkItem -TitleBackgroundColor Grey -BoldTopRow -AutoFilter -FreezeTopRow -WorksheetName Coaches}catch{write-error "Unrecoverable Error - Exception thrown"}
+#RawData sheet
+try{$targettedmeetings | Select -Property "host","email","topic","start_time","end_time","duration","participants" | export-excel $WorkItem -TitleBackgroundColor Grey -BoldTopRow -AutoFilter -FreezeTopRow -WorksheetName RawData}catch{write-error "Unrecoverable Error - Exception thrown"}
 
 # Prompts user if they want a paste to the console of the zoom results
 $PastePrompt = read-host "Would you like to see a paste of the results? (Y/N)"
 if ($PastePrompt -like "Y"){
     $targettedmeetings | format-table -AutoSize
 }
+write-host "Modifying Excel Sheet..."
 
-Read-host "Press any key to continue..." | Out-Null
+#Creates an Excel COM Object to modify the Excel Sheet we just made
+$Excel = New-Object -ComObject Excel.Application
+$Workbook = $Excel.Workbooks.Open($WorkItem)
+$workSheet = $Workbook.Worksheets.Item(1)
+
+#Autofit Columns on the Coaches sheet
+$worksheet.Columns.Autofit()
+$rows = $workSheet.range("D1").currentregion.rows.count
+
+#Applies Standardized date Format for both Beginning and Ending time (2018-10-03T14:25:01Z >> 10/3/18 17:58)
+$worksheet.range("D2:D$rows").formula = "=DATEVALUE(MID(RawData!D2,1,10))+TIMEVALUE(MID(RawData!D2,12,8))"
+$worksheet.range("D2:D$rows").NumberFormat = "m/d/yy h:mm"
+$worksheet.range("E2:E$rows").formula = "=DATEVALUE(MID(RawData!E2,1,10))+TIMEVALUE(MID(RawData!E2,12,8))"
+$worksheet.range("E2:E$rows").NumberFormat = "m/d/yy h:mm"
+
+#Add new Totals Worksheet
+$WS = $Workbook.worksheets.Add()
+$WS.name = "Totals"
+$WS.Columns.Autofit()
+
+#Freezes the top row, and attempts to autofit the columns
+$excel.Rows.Item("2:2").Select()
+$excel.ActiveWindow.FreezePanes = $true
+
+#Adds data for the host column
+$WS.cells.item(1,1).value2 = "Host"
+    #Add each User
+    $num = 2
+    foreach ($user in $userlist){
+        $WS.Cells.item($num,1).value2 = "$user"
+        $num = $num + 1}
+
+#Adds data for the Total Meetings Column (takes the total sum of each of the user's meetings
+$WS.cells.item(1,2).value2 = "Total Meetings"
+    $num = 2
+    foreach ($user in $userlist){
+        $Ws.Cells.item($num,2).formula = "=COUNTIF(Coaches!B2:B$rows,""$user"")"
+        $num = $num + 1}
+
+#Adds data for the Total Time Column (I don't think there is a way to actually get the total at the moment)
+$WS.cells.item(1,3).value2 = "Total Time"
+    $num = 2
+    foreach ($user in $userlist){
+        $Ws.Cells.item($num,3).value2 = "#"
+        $num = $num + 1
+        }
+
+#Adds data for the average duration (Takes the total time for meetings, and divides it by the total number of meetings)
+$WS.cells.item(1,4).value2 = "Average Duration"
+    $num = 2
+       foreach ($user in $userlist){
+       $Ws.Cells.item($num,4).formula = "=Totals!C$num/Totals!B$num"
+       $num = $num + 1
+       }
+
+#Show user the finished workbook
+$excel.Visible = $true
